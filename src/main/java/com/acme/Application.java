0f363211d2ccc9996e91ab5d87ade95b60dac7e3
Brian <38346591+brianereynolds@users.noreply.github.com>
2018-04-13 10:46:22 +0100

    Added Application

---
+++ src/main/java/com/acme/Application.java
@@ -0,0 +1,16 @@
+package com.acme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Bootstrap class for Spring Boot
+ *
+ * @author Brian.e.reynolds@outlook.com
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
